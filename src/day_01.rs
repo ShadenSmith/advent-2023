0cc01d64b01cc8b1c827ed8e12164981b9104cd4
Shaden Smith <shadentsmith@gmail.com>
2023-12-02 13:49:17 -0800

    starting day2 parsing

--- src/day_01.rs
+++ src/day_01.rs
@@ -18,6 +18,33 @@ fn extract_digits(line: &str) -> Option<u64> {
     Some(num)
 }
 
+/// Parse a lowercase English digit: "one", "two", ... "nine"
+fn parse_digit(input: &str) -> Option<u64> {
+    match input {
+        "one" => Some(1),
+        "two" => Some(2),
+        "three" => Some(3),
+        "four" => Some(4),
+        "five" => Some(5),
+        "six" => Some(6),
+        "seven" => Some(7),
+        "eight" => Some(8),
+        "nine" => Some(9),
+        _ => None,
+    }
+}
+
+fn extract_more_digits(line: &str) -> Option<u64> {
+    if line.is_empty() {
+        return None;
+    }
+
+    // first try to match a digit
+    //line.chars().into_iter().fir
+
+    None
+}
+
 pub fn day_01_a(path_name: &str) -> u64 {
     let reader = BufReader::new(fs::File::open(path_name).unwrap());
 
@@ -26,6 +53,18 @@ pub fn day_01_a(path_name: &str) -> u64 {
         .map(|line| {
             let line = line.unwrap();
             extract_digits(&line).expect("No digits found in line")
+        })
+        .sum()
+}
+
+pub fn day_01_b(path_name: &str) -> u64 {
+    let reader = BufReader::new(fs::File::open(path_name).unwrap());
+
+    reader
+        .lines()
+        .map(|line| {
+            let line = line.unwrap();
+            extract_more_digits(&line).expect("No digits found in line")
         })
         .sum()
 }
@@ -44,8 +83,33 @@ mod tests {
     }
 
     #[test]
-    fn test_day1() {
+    fn test_extract_more_digits() {
+        assert_eq!(extract_more_digits("cats"), None);
+        assert_eq!(extract_more_digits("two1nine"), Some(29));
+        assert_eq!(extract_more_digits("eightwothree"), Some(83));
+        assert_eq!(extract_more_digits("abcone2threexyz"), Some(13));
+        assert_eq!(extract_more_digits("xtwone3four"), Some(24));
+        assert_eq!(extract_more_digits("4nineeightseven2"), Some(42));
+        assert_eq!(extract_more_digits("zoneight234"), Some(14));
+        assert_eq!(extract_more_digits("7pqrstsixteen"), Some(76));
+    }
+
+    #[test]
+    fn test_parse_digit() {
+        assert_eq!(parse_digit("one"), Some(1));
+        assert_eq!(parse_digit("five"), Some(5));
+        assert_eq!(parse_digit("sixteen"), None);
+    }
+
+    #[test]
+    fn test_part1() {
         let result = day_01_a("inputs/day_01_test.txt");
         assert_eq!(result, 142);
+    }
+
+    #[test]
+    fn test_part2() {
+        let result = day_01_b("inputs/day_01_test.txt");
+        assert_eq!(result, 281);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use advent::*;
 
 fn day1() {
     let part1 = day_01::day_01_a("inputs/day_01.txt");
+    let part2 = day_01::day_01_b("inputs/day_01.txt");
     println!("Day 01");
     println!("  Part 1: {}", part1);
+    println!("  Part 2: {}", part2);
 }
 
 fn main() {
