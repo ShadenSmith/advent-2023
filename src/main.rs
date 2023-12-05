0bfec369dc6a77d9ab8958f4812e3cd7472a13d8
Shaden Smith <shadentsmith@gmail.com>
2023-12-05 10:59:12 -0800

    remove full input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use advent::*;
 
 fn day1() {
-    let part1 = day_01::day_01_a("inputs/day_01.txt");
-    let part2 = day_01::day_01_b("inputs/day_01.txt");
+    let part1 = day_01::day_01_a("full-inputs/day_01.txt");
+    let part2 = day_01::day_01_b("full-inputs/day_01.txt");
     println!("Day 01");
     println!("  Part 1: {}", part1);
     println!("  Part 2: {}", part2);
